ea905f2043ba9aaceef358eafc515faaf88aea19
rahul-sahane <rahulsahane778@gmail.com>
2026-08-17 23:30:27 +0530

    Initial commit: swiggy-user-service Spring Boot project

---
+++ swiggy-user-service/src/main/java/com/swiggy/userservice/SwiggyUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.swiggy.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiggyUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiggyUserServiceApplication.class, args);
+	}
+
+}
---
+++ swiggy-user-service/src/main/java/com/swiggy/userservice/entity/User.java
@@ -0,0 +1,63 @@
+package com.swiggy.userservice.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity 					   // Tells Spring: "this class = a database table"
+@Table(name = "users")		  //  The actual table name in Oracle 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	
+	@Id
+	@Column(name = "id")
+	private long id;
+	
+	@Column(name = "full_name", nullable = false, length = 100)
+	private String fullName;
+	
+	@Column(name = "email", nullable = false, length = 100)
+	private String email;
+	
+	@Column(name = "password_hash", nullable = false, length = 255)
+	private String password;
+	
+	@Column(name = "phone_number", nullable = false, length = 20)
+	private String phoneNumber;
+	
+	@Column(name = "role", nullable = false, length = 30)
+	private String role;
+	
+	@Column(name = "is_acive", nullable = false)
+	private boolean isActive;
+	
+	@Column(name = "created_at", nullable = false, updatable = false)
+	LocalDateTime createdAt;
+	
+	@Column(name = "updated_at", nullable = false)
+	LocalDateTime updatedAt;
+	
+	
+	
+	 // These two run AUTOMATICALLY before saving/updating
+	@PrePersist									// Runs just BEFORE a new row is inserted
+	protected void OnCreate() {
+		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+	}
+	
+	@PreUpdate									// Runs just BEFORE an existing row is updated
+	protected void OnUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+}
