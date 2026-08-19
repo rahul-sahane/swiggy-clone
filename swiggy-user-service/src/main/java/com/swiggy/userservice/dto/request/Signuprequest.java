440e34f1e8e4ebf5e8b874193768456e1ae1ab51
rahul-sahane <rahulsahane778@gmail.com>
2026-08-20 00:37:27 +0530

    Add Signup Request DTO And Login Request DTO with validations

---
+++ swiggy-user-service/src/main/java/com/swiggy/userservice/dto/request/Signuprequest.java
@@ -0,0 +1,34 @@
+package com.swiggy.userservice.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class Signuprequest {
+	
+	@NotBlank(message = "Full name is required")
+	@Size(min = 2, max = 150, message = "Full name must between 2 to 150")
+	private String fullName;
+	
+	@NotBlank(message = "Email is required")
+	@Email(message = "Invalid email format")
+	private String email;
+	
+	@NotBlank(message = "Password is required")
+	@Size(min = 6, message = "Password must be at least 6 character")
+	private String password;
+	
+	@NotBlank(message = "Phone numbe is required")
+	@Pattern(regexp = "^[0-9]{10}$" , message = "Phone number must be exactly 10 digit")
+	private String phoneNumber;
+	
+	@NotBlank(message = "Role is required")
+	@Pattern(
+			regexp = "CUSTOMER | RESTAURENT_OWNER | DELIVERY_PARTNER | ADMIN",
+			message = "Role must be one of: CUSTOMER, RESTAURANT_OWNER, DELIVERY_PARTNER, ADMIN"
+			)
+	private String role;
+}
