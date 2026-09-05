744c5dac83dbd84f5a4f7bbecb5c03cc3b506779
rahul-sahane <rahulsahane778@gmail.com>
2026-09-05 23:53:53 +0530

    Start swiggy-restaurant-service: Restaurant and MenuItem entities, repositories, Flyway migration

---
+++ swiggy-restaurant-service/src/main/java/com/swiggy/restaurantservice/SwiggyRestaurantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.swiggy.restaurantservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiggyRestaurantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiggyRestaurantServiceApplication.class, args);
+	}
+
+}
---
+++ swiggy-restaurant-service/src/test/java/com/swiggy/restaurantservice/SwiggyRestaurantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiggy.restaurantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiggyRestaurantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
